64d5cfe65fb16acf164dfdee56f97a811934e741
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2013-04-05 08:35:34 +0000

    opendingux: GCW0: Create /etc/local/shadow on boot if absent

--- board/opendingux/gcw0/target_skeleton/etc/init.d/S15localfsinit.sh
+++ board/opendingux/gcw0/target_skeleton/etc/init.d/S15localfsinit.sh
@@ -7,6 +7,10 @@ if [ -z "$1" ] || [ "x$1" = "xstart" ]; then
         mkdir -p /usr/local/$i
     done
 
+    if [ ! -f /etc/local/shadow ]; then
+        cp /etc/shadow_skel /etc/local/shadow
+    fi
+
     if [ ! -f /etc/local/timezone ]; then
         echo 'CET-1CEST,M3.5.0,M10.5.0' > /etc/local/timezone
     fi
